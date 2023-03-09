e2131f51193ffe4686b88fd894db23362aedefb9
mdcrowley <matthew.crowley@tri-c.edu>
2023-03-08 20:29:21 -0700

    Add files via upload

---
+++ freelib.js
@@ -0,0 +1,17 @@
+//////////////////////
+// Global Variables //
+//////////////////////
+
+const bookOne = {
+   title: "Corduroy",
+   author: "Don Freeman",
+   publisher: "Viking Books",
+   year: "1968",
+   jacketColor: "firebrick"
+}
+
+///////////////
+// Functions //
+///////////////
+
+// Write your function(s) here
